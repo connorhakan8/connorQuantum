3c7d179e3964b52bea089093e5107d32a33628ae
Connor Williams <connorhakan8@gmail.com>
2024-12-05 00:32:42 -0500

    more basic tinkering with writing the program

--- Quantum_Circuit_Design/qlearning_recreation.py
+++ Quantum_Circuit_Design/qlearning_recreation.py
@@ -1,3 +1,8 @@
+#this ia a mix of me trying to figure stuff out generally on my own
+#i sometimes go back to the original file for inspiration on how to approach something
+#other times i just write pseudocode-like code (should look like python code, but might not run)
+
+
 import gym
 from gym import spaces
 import hashlib
@@ -54,8 +59,14 @@ class QuantumEnv(gym.Env):
         
     
 class QLearningAgent:
-    def __init__(self):
-        print("This is a placeholder method.")
+    def __init__(self, learning_rate, discount_factor, exploration_prob, state_size, action_size):
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.exploration_prob = exploration_prob
+        self.state_size = state_size
+        self.action_size = action_size
+        #we make a table filled with zeros that has dimensions state_size x action_size
+        self.q_table = np.zeros((state_size, action_size))
 
     #all the possible moves. We add a specific gate (h gate, t gate, etc)
     def _move(self, action):
@@ -77,7 +88,25 @@ class QLearningAgent:
 
         ]
 
+        if np.random.rand() < self.exploration_prob:
+            action = np.random.randint(0, self.state_size)
+        else:
+            action = np.argmax(self.q_table[current_action])
+        
+
     def _step(self):
-        print("This is a placeholder method that might go here later.")
+        print("Placeholder")
 
+    #rudimentary reward function
+    def _reward(self):
+        if(current_state == target_state):
+            reward = 10
+        else:
+            reward = -1
 
+    def _update_q_table(): #formula from geeksforgeeks, somewhat adopted for this program
+        self.q_table[self.current_state, self.action] += self.learning_rate * (self.reward + self.discount_factor * np.max(self.q_table[next_state]) - self.q_table[self.current_state, self.action])
+
+            
+    #current status: getting a good idea on how reinforcement learning works,
+    #having trouble implementing this for a quantum system.
